31356a52094903a14450689b6c87fac7f0f4b794
tobyrushton <tobyjrushton@icloud.com>
2024-03-18 21:30:10 +0000

    created e2e for modules

---
+++ __tests__/e2e/helpers/data.ts
@@ -0,0 +1,30 @@
+export const modules = [
+    {
+        code: 'COMP1012',
+        name: 'Introduction to Programming',
+        grade: 84,
+        credits: 10,
+    },
+    {
+        code: 'COMP1211',
+        name: 'Computer Architecture',
+        grade: 66,
+        credits: 10,
+    },
+    {
+        code: 'COMP1421',
+        name: 'Fundamental Mathematical Concepts',
+        grade: 73,
+        credits: 10,
+    },
+    {
+        code: 'COMP1711',
+        name: 'Procedural Programming',
+        credits: 10,
+    },
+    {
+        code: 'COMP1911',
+        name: 'Professional Computing',
+        credits: 20,
+    },
+]
